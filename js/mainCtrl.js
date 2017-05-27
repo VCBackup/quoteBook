3ee1b4b8c397b69f98c60b61e951bd8414a27f98
Moore <DMoore@isolvedhcm.com>
2017-05-27 10:33:46 -0600

    Completed assignment, attempted to add localStorage but failed.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,22 @@
 angular.module('quoteBook').controller('mainCtrl', function($scope, mainService){
-    
-    $scope.test = "Hello World!";
 
     $scope.quotes = mainService.getQuotes();
+
+    $scope.deleteMe = function(textToDelete){
+        mainService.removeData(textToDelete);
+    }
+
+    $scope.addQuote = function(){
+        var newQuote = {
+            text: $scope.newQuoteText,
+            author: $scope.newQuoteAuthor
+        }
+        if (mainService.addData(newQuote)){
+            $scope.newQuoteText = '';
+            $scope.newQuoteAuthor = '';
+        }
+    }
+
+    $scope.savedQuote = mainService.saveData();
+
 });
